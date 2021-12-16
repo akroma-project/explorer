1e6bf1841b1bee7dd4f8d871e1c62b54eb7084b6
detroitpro <eric@polerecky.com>
2021-12-15 21:48:04 -0500

    feat: add support for test-network browsing

--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -1,19 +1,27 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 @Injectable()
 export class SettingsService {
-    constructor() {}
+  constructor() {}
 
-    public getConnectionUrl(): string {
-        const url = localStorage.getItem('connection_url');
-        if (url !== null) {
-            return url;
-        }
-        return 'https://boot2.akroma.org';
+  public getConnectionUrl(): string {
+    const url = localStorage.getItem("connection_url");
+    if (url !== null) {
+      return url;
     }
+    return "https://boot2.akroma.org";
+  }
 
-    public setConnectionUrl(url: string): string {
-        // TODO: validation.
-        localStorage.setItem('connection_url', url);
-        return url;
+  public setConnectionUrl(url: string): string {
+    switch (url) {
+      case "prod":
+        localStorage.setItem("connection_url", "https://boot2.akroma.org");
+        break;
+      case "test":
+        localStorage.setItem("connection_url", "https://test1.akroma.org");
+        break;
+      default:
+        break;
     }
+    return url;
+  }
 }
