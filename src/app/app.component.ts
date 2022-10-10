ec260a7251cbbcd2d89fe654cb13c39c72ccacba
Eric Polerecky <eric@polerecky.com>
2022-10-09 20:36:33 -0400

    fix: build (#16)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,8 @@
 import { Component } from "@angular/core";
-import { BlockService } from "./services/block.service";
-
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  constructor(private blockService: BlockService) {}
-
-  async ngOnInit() {
-    // await this.blockService.getBlocks();
-    
-  }
 }
